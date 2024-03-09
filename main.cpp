e83c63f0a01b107e18605409efed4c80f096b494
Ricky Valentine Oswald <rickyoswald@gmail.com>
2024-03-09 20:19:45 +0000

    refactored code

--- main.cpp
+++ main.cpp
@@ -10,19 +10,14 @@ int main (int argc, char *argv[]) {
   std::srand(std::time(nullptr));
 
   std::string uuid ("", UUID_LENGTH);
-  for (size_t i {0}; i < UUID_LENGTH; ++i) {
-    int r = std::rand() /  ((RAND_MAX + 1u) / 16);
-    std::cout << UUID_CHARS.at(r) << ' ';
-    uuid.at(i) = UUID_CHARS.at(r);
-  }
-
+  for (size_t i {0}; i < UUID_LENGTH; ++i)
+    uuid.at(i) = UUID_CHARS.at(std::rand() /  ((RAND_MAX + 1u) / 16));
   uuid.insert(20, "-");
   uuid.insert(16, "-");
   uuid.insert(12, "-");
   uuid.insert(8, "-");
 
-  std::cout << std::endl << uuid;
+  std::cout << uuid << std::endl;
   
-  std::cout << std::endl;
   return 0;
 }
